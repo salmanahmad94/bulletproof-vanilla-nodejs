a022c909253e3b62fee721b69313730438917cdc
Salman Ahmad <salmanahmad94@gmail.com>
2021-12-24 21:32:39 +0500

    Configure pm2 for production mode

--- src/app.js
+++ src/app.js
@@ -1,36 +1,39 @@
-import express from 'express';
+import regeneratorRuntime from "regenerator-runtime";
+import express from "express";
 
-import loaders from 'src/loaders';
-import shutdown from 'src/shutdown';
+import loaders from "src/loaders";
+import shutdown from "src/shutdown";
 
-import config from 'config';
-import Logger from 'config/winston';
+import config from "config";
+import Logger from "config/winston";
 
 async function init() {
-	// Create a new express instance.
+  // Create a new express instance.
 
-	const app = express();
+  const app = express();
 
-	// Create a server variable and load the app components with the new express instance.
+  // Create a server variable and load the app components with the new express instance.
 
-	let server = await loaders(app);
+  let server = await loaders(app);
 
-	// If the server is null, crash the app.
+  // If the server is null, crash the app.
 
-	if (!server) {
-		return Logger.error("HTTP server failed to start at port: " + config.app.port);
-	}
+  if (!server) {
+    return Logger.error(
+      "HTTP server failed to start at port: " + config.app.port
+    );
+  }
 
-	// Start the app at the PORT defined.
+  // Start the app at the PORT defined.
 
-	server = app.listen(config.app.port, err => {
-		if (err) process.exit(1);
-		Logger.info("HTTP server started at port: " + config.app.port);
-	});
+  server = app.listen(config.app.port, (err) => {
+    if (err) process.exit(1);
+    Logger.info("HTTP server started at port: " + config.app.port);
+  });
 
-	// Attach graceful shutdown event listener.
+  // Attach graceful shutdown event listener.
 
-	await shutdown(server);
+  await shutdown(server);
 }
 
 init();
